07e056210e99d74746fda01e17ffe67b83ac3183
cdmaji1 <cdmaji1@jd.com>
2018-01-01 16:57:49 +0800

    add http link for ffmpeg commands

--- ffmpeg_splits.sh
+++ ffmpeg_splits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #ffmpeg -i test.mp4 -c:v libx264 -c:a aac  -strict -2 -f hls ./video_output/output.m3u8 hls_time 5  
-
+#http://elkpi.com/topics/ffmpeg-f-hls.html
 ffmpeg -i test.mp4 -c copy -map 0 -f segment -segment_list playlist.m3u8 -segment_time 5 ./video_output/output%03d.ts  
 
